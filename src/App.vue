674105c9f804f09584c42afd1d77a2ec1a0fa9fc
Carles Serna <carles.serna@gmail.com>
2019-01-03 18:51:29 +0100

    styling videos

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+<style src="./assets/scss/index.scss" lang="scss">
+
 </style>
