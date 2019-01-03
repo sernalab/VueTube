a803ec8e5d4583aca37a6f984bbcbcccf0073e46
Carles Serna <carles.serna@gmail.com>
2019-01-03 17:41:27 +0100

    creating data

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <VideoPlayer/>
+  </div>
+</template>
+
+<script>
+import VideoPlayer from './components/VideoPlayer'
+
+export default {
+  name: 'App',
+  components: {
+    VideoPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="video-player">
+        Welcome to VueTube
+    </div>
+</template>
+
+<script>
+let videos = [{
+        id: 1,
+        title:'Macie Jay Thermite',
+        thumbnail: 'https://img.youtube.com/vi/HRdeN5tTHBk/0.jpg',
+        youtubeURL: 'https://www.youtube.com/watch?v=HRdeN5tTHBk',
+        creator: 'Macie Jay',
+        likes: 0,
+        views: 0
+    },
+    {
+        id: 2,
+        title:'Zakk Wylde',
+        thumbnail: 'https://img.youtube.com/vi/TgPhUNZ3db4/0.jpg',
+        youtubeURL: 'https://www.youtube.com/watch?v=TgPhUNZ3db4',
+        creator: 'Zakk Wylde',
+        likes: 0,
+        views: 0 
+    },
+    {
+        id: 3,
+        title:'Linus Laptop',
+        thumbnail: 'https://img.youtube.com/vi/RaLMgJA1HKc/0.jpg',
+        youtubeURL: 'https://www.youtube.com/watch?v=RaLMgJA1HKc',
+        creator: 'Linus Tech Tips',
+        likes: 0,
+        views: 0 
+    },
+    {
+        id: 4,
+        title:'Razer Headset',
+        thumbnail: 'https://i.ytimg.com/vi/DvnSRj8xQBI/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAsdihH_sWaTJpIC7M2fX94wFBRag',
+        youtubeURL: 'https://www.youtube.com/watch?v=DvnSRj8xQBI&t=297s',
+        creator: 'Linus Tech Tips',
+        likes: 0,
+        views: 0 
+    },
+    {
+        id: 5,
+        title:'Metallica Guitar Talk with James',
+        thumbnail: 'https://i.ytimg.com/an_webp/nKHAQSyMTMo/mqdefault_6s.webp?du=3000&sqp=CNyDuOEF&rs=AOn4CLCsB0dGcUb1BmQL4pMO2uZoeRsCyQ',
+        youtubeURL: 'https://www.youtube.com/watch?v=nKHAQSyMTMo',
+        creator: 'Metallica TV',
+        likes: 0,
+        views: 0 
+    },
+    {
+        id: 6,
+        title:'Metallica: Orion live 2018',
+        thumbnail: 'https://img.youtube.com/vi/iL42fwM5muE/0.jpg',
+        youtubeURL: 'https://youtu.be/iL42fwM5muE',
+        creator: 'Metallica TV',
+        likes: 0,
+        views: 0 
+    }];
+export default {
+    name: 'VideoPlayer',
+    data () {
+        return {
+            videos
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
